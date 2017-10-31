10f4c1d5e8c3ab11196e5d8184029feac5893262
p_busko <p_busko@wargaming.net>
2017-10-31 18:33:03 +0300

    fix issue 1403 by introducing new flag: PCF_IN_CTOR_CALL

--- src/uncrustify_types.h
+++ src/uncrustify_types.h
@@ -159,6 +159,7 @@ struct parse_frame_t
 #define PCF_IN_NAMESPACE       PCF_BIT(12)
 #define PCF_IN_FOR             PCF_BIT(13)
 #define PCF_IN_OC_MSG          PCF_BIT(14)
+#define PCF_IN_CTOR_CALL       PCF_BIT(15)
 
 // Non-Copy flags are in the upper 48 bits
 #define PCF_FORCE_SPACE        PCF_BIT(16)  //! must have a space after this token
